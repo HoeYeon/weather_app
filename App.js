a17e28f376c5880ee5f05b8acf18a66c28e34fec
Hoeyeon <shy9546@naver.com>
2020-11-19 23:47:07 +0900

    add get weather information
    hide api key

--- App.js
+++ App.js
@@ -2,10 +2,18 @@ import React from "react";
 import Loading from "./Loading";
 import { Alert, View, Text } from "react-native";
 import * as Location from "expo-location";
+import axios from "axios";
+import { APIkey } from "./apikey";
 
 export default class extends React.Component {
   state = {
     isLoading: true,
+  };
+  getWeather = async (latitude, longitude) => {
+    const data = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIkey}&units=metric`
+    );
+    console.log(data);
   };
   getLocation = async () => {
     try {
@@ -13,6 +21,7 @@ export default class extends React.Component {
       const {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("Can't find ypu");
@@ -21,6 +30,7 @@ export default class extends React.Component {
   componentDidMount() {
     this.getLocation();
   }
+
   render() {
     const { isLoading } = this.state;
     return isLoading ? (
